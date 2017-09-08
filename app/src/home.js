b906b44d46c25d198d1300ae24576d7edadbaead
arjaynguyen <arjaynguyen123@hotmail.com>
2017-09-08 13:06:43 -0700

    splash screen implemented

---
+++ app/src/home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './home.css';
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div id="home">
+                <nav>
+                    <a className="nav-link" href="#">Link</a>
+                    <a className="nav-link" href="#">Link</a>
+                    <a className="nav-link" href="#">Link</a>
+                    <a className="nav-link" href="#">Link</a>
+                    <a className="nav-link" href="#">Link</a>
+                </nav>
+                <header>
+                    <h1 className="hdr-text">Arjay Nguyen</h1>
+                    <h2 className="hdr-text">This text goes underneath</h2>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Home;
--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './home';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Page extends React.Component {
+    render() {
+        return(
+            <div id="app">
+                <Home />
+                <App />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Page />, document.getElementById('root'));
 registerServiceWorker();
