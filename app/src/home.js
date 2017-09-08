5ba2c74c925cbf1abbb4fb89de4ec10310c96b81
arjaynguyen <arjaynguyen123@hotmail.com>
2017-09-08 15:49:57 -0700

    about section implemented

--- app/src/home.js
+++ app/src/home.js
@@ -6,15 +6,14 @@ class Home extends React.Component {
         return(
             <div id="home">
                 <nav>
-                    <a className="nav-link nav-link--current" href="#">Link</a>
-                    <a className="nav-link" href="#">Link</a>
-                    <a className="nav-link" href="#">Link</a>
-                    <a className="nav-link" href="#">Link</a>
-                    <a className="nav-link" href="#">Link</a>
+                    <a className="nav-link" href="#">Home</a>
+                    <a className="nav-link" href="#">About</a>
+                    <a className="nav-link" href="#">Projects</a>
+                    <a className="nav-link" href="#">Contact</a>
                 </nav>
-                <header>
-                    <h1 className="hdr-text">Arjay Nguyen</h1>
-                    <h5 className="hdr-text">Descriptive Text</h5>
+                <header className="home-hdr">
+                    <h1 className="hdr-text hdr-hero">Arjay Nguyen</h1>
+                    <h5 className="hdr-text hdr-support">Descriptive Text</h5>
                 </header>
             </div>
         );
