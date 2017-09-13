74697fecdcc6968f1ea841b4a038ba75fbc6cd87
arjaynguyen <arjaynguyen123@hotmail.com>
2017-09-13 09:15:47 -0700

    styling and images

--- app/src/projects.js
+++ app/src/projects.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './projects.css';
+import recipebook from './recipebook-large.png';
+import neighborhoodmap from './neighborhoodmap-large.png';
+import itemcatalog from './itemcatalog-large.png';
+import multiuserblog from './multiuserblog-large.png';
 
 class Project extends React.Component {
     render() {
         return(
             <div className='project'>
-                <div className="project-wrap">
-                <img src={this.props.img} />
+                <div className='project-wrap'>
+                <a href={this.props.link}>
+                    <div className='img-wrap'>
+                        <img className='project-img' src={this.props.img} />
+                    </div>
+                </a>
                 <h2>{this.props.title}</h2>
                 <p>{this.props.desc}</p>
                 </div>
@@ -15,44 +23,47 @@ class Project extends React.Component {
     }
 }
 
+
 const projects = [
     {
-        'title': 'Title',
-        'img': 'http://placehold.it/200/200',
-        'desc': 'Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will work correctly both with client-side routing and a non-root public URL.'
+        'title': 'Recipe Book',
+        'img': recipebook,
+        'desc': 'Lorem ipsum dolor sit amet, eu saperet commune voluptua quo, atomorum gubergren reformidans no pri. Et qui error ocurreret liberavisse. Alii delenit sit ne, ex vis congue causae tacimates. Tibique accusata per eu. Eu quo mucius fabellas inimicus, appellantur complectitur an duo, vix cu utinam partem.',
+        'link': '#'
     },
     {
-        'title': 'Title',
-        'img': 'http://placehold.it/200/200',
-        'desc': 'Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will work correctly both with client-side routing and a non-root public URL.'
+        'title': 'Neighborhood Map',
+        'img': neighborhoodmap,
+        'desc': 'Lorem ipsum dolor sit amet, eu saperet commune voluptua quo, atomorum gubergren reformidans no pri. Et qui error ocurreret liberavisse. Alii delenit sit ne, ex vis congue causae tacimates. Tibique accusata per eu. Eu quo mucius fabellas inimicus, appellantur complectitur an duo, vix cu utinam partem.',
+        'link': '#'
     },
     {
-        'title': 'Title',
-        'img': 'http://placehold.it/200/200',
-        'desc': 'Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will work correctly both with client-side routing and a non-root public URL.'
+        'title': 'Item Catalog',
+        'img': itemcatalog,
+        'desc': 'Lorem ipsum dolor sit amet, eu saperet commune voluptua quo, atomorum gubergren reformidans no pri. Et qui error ocurreret liberavisse. Alii delenit sit ne, ex vis congue causae tacimates. Tibique accusata per eu. Eu quo mucius fabellas inimicus, appellantur complectitur an duo, vix cu utinam partem.',
+        'link': '#'
     },
     {
-        'title': 'Title',
-        'img': 'http://placehold.it/200/200',
-        'desc': 'Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will work correctly both with client-side routing and a non-root public URL.'
-    },
-    {
-        'title': 'Title',
-        'img': 'http://placehold.it/200/200',
-        'desc': 'Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will work correctly both with client-side routing and a non-root public URL.'
+        'title': 'Multi User Blog',
+        'img': multiuserblog,
+        'desc': 'Lorem ipsum dolor sit amet, eu saperet commune voluptua quo, atomorum gubergren reformidans no pri. Et qui error ocurreret liberavisse. Alii delenit sit ne, ex vis congue causae tacimates. Tibique accusata per eu. Eu quo mucius fabellas inimicus, appellantur complectitur an duo, vix cu utinam partem.',
+        'link': '#'
     }
 ];
 
 class Projects extends React.Component {
     render() {
         var proj_render = projects.map((project, index) =>
-            <Project key={index} title={project.title} img={project.img} desc={project.desc}/>
+            <Project key={index} title={project.title} img={project.img} desc={project.desc} link={project.link}/>
         );
 
         return(
             <div id='projects'>
                 <div className='wrap'>
-                    {proj_render}
+                    <h1>Projects</h1>
+                    <div className="projects-wrap">
+                        {proj_render}
+                    </div>
                 </div>
             </div>
         );
