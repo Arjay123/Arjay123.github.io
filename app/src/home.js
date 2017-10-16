a937c0de9963d2e8803eca258570729ddb337a11
arjaynguyen <arjaynguyen123@hotmail.com>
2017-10-16 12:32:38 -0700

    add navbar background drop down

--- app/src/home.js
+++ app/src/home.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './home.css';
+import $ from 'jquery';
 
 class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    handleScroll() {
+
+        const nav = $('#nav');
+        const navHeight = nav.height() +
+                          parseInt(nav.css('margin-top'), 10) +
+                          parseInt(nav.css('margin-bottom'), 10);
+        const marker = $('#home').height() - navHeight;
+
+        if($(window).scrollTop() >= marker){
+            $('#nav-back').slideDown(200);
+        }
+        else {
+            $('#nav-back').slideUp(200);
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
     render() {
         return(
             <div id="home">
-                <nav>
+                <div id="nav-back" className="hidden"></div>
+                <nav id="nav">
                     <a className="nav-link" href="#">Home</a>
                     <a className="nav-link" href="#">About</a>
                     <a className="nav-link" href="#">Projects</a>
